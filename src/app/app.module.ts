331e4c1056f60f57bc964975fa2b4007f2074827
Theo Henrique <pereiratheo07@gmail.com>
2025-06-04 13:51:38 -0300

    feat: add AngularFire for Firebase integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +16,9 @@ import { LoginComponent } from './components/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAIF_RH4xi5WcCyZYaCYHale7X17JWAXBs",
+    authDomain: "asimovie.firebaseapp.com",
+    projectId: "asimovie",
+    storageBucket: "asimovie.firebasestorage.app",
+    messagingSenderId: "75683719274",
+    appId: "1:75683719274:web:8da3d2a5e71584f8140213",
+    measurementId: "G-81Y6T3X0KL"
+  }
+}
