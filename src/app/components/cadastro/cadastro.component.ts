f026165a670a46081b6460468e9b51f5199e3523
Theo Henrique <pereiratheo07@gmail.com>
2025-06-04 14:15:40 -0300

    Add new logo, configure Tailwind CSS and multiple configurations

---
+++ src/app/components/cadastro/cadastro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro',
+  standalone: false,
+  templateUrl: './cadastro.component.html',
+  styleUrl: './cadastro.component.scss'
+})
+export class CadastroComponent {
+
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,ts}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'black': '#000000',
+      },
+    },
+  },
+  plugins: [],
+}
