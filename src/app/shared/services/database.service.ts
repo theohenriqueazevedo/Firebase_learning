1a436335a405925f93363db7da0d19d40b76f56a
Theo Henrique <pereiratheo07@gmail.com>
2025-06-05 23:29:28 -0300

    fix: update database service methods to include return types and clean up syntax

--- src/app/shared/services/database.service.ts
+++ src/app/shared/services/database.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 
 
 
@@ -11,30 +12,26 @@ export class DatabaseService {
   constructor(private db: AngularFirestore) { }
 
   //funcoes que irao interagir com o banco de dados
-  addDocument(collection:string, data:any){
-    
+  addDocument(collection:string, data:any): Promise<any>{
+    return this.db.collection(collection).add(data)
+      
   }
-  getDocument(collection:string,)){
-
+  getDocument(collection:string, id: string): Observable<any | undefined> {
+    return this.db.collection(collection).doc(id).valueChanges();
   }
-  getCollection(collection:string,)){
-
+  getCollection(collection:string): Observable<any[]> {
+    return this.db.collection(collection).valueChanges({idField:'id'});
   }
-  getCollectionWithFilter(collection:string,)){
-
-  }
-  updateDocument(collection:string,)){
-
-  }
-  deleteDocument(collection:string,)){
+  getCollectionWithFilter(collection:string, field:string, value: string): Observable<any[]> {
+    return this.db.collection(collection, ref => ref.where(field, '==', value)).valueChanges({idField: 'id'});
 
   }
-  
+  updateDocument(collection:string,){
 
-
-
-
-
+  }
+  deleteDocument(collection:string, id:string): Promise<void> {
+    return this.db.collection(collection).doc(id).delete();
+  }
 
 
 
